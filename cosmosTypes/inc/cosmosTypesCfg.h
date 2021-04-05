1539a8ea5e2f4936813b334f984cbaccdcde975e
PavolKostolansky <pavol.kostolansky@gmail.com>
2021-04-05 21:46:09 +0200

    capitalization update

---
+++ cosmosTypes/inc/cosmosTypesCfg.h
@@ -0,0 +1,267 @@
+/********************************************************************************
+**                                                                             **
+**                         COSMOS FILE | CREATED BY HUMAN                      **
+**                                                                             **
+*********************************************************************************
+**                       DOXYGEN DOCUMENTATION INFORMATION                     **
+*****************************************************************************//**
+** @file cosmosTypesCfg.h                                      
+*********************************************************************************	
+<!--                   cosmosTypesCfg Unit Group Definition                   -->
+********************************************************************************* 
+** @defgroup cosmosTypesCfg_unit cosmosTypesCfg Unit 
+** @ingroup cosmosTypes_module        
+** @brief cosmosTypesCfg Unit 
+** @details lorem                               
+*********************************************************************************	
+<!--                           Version Information                            -->
+*********************************************************************************
+** @version 1.0.0
+** @date 1.8.2020
+** @author https://github.com/PavolKostolansky   
+*********************************************************************************	
+<!--                          Warnings and License                            -->
+*********************************************************************************
+** @warning Modifying code can lead to unexpected behaviour of the whole system
+** @copyright MIT License       	
+*********************************************************************************
+<!--                cosmosTypesCfg Unit Global Group Definition               -->
+*********************************************************************************	
+** @defgroup Global_cosmosTypesCfg Global
+** @ingroup cosmosTypesCfg_unit 
+** @brief cosmosTypesCfg globals
+** @details lorem 
+********************************************************************************/ 
+/********************************************************************************
+**                           START OF THE HEADER FILE                          **
+********************************************************************************/
+#ifndef __COSMOSTYPESCFG_H__
+#define __COSMOSTYPESCFG_H__
+/********************************************************************************
+**                         START OF C++ SUPPORT SECTION                        **
+********************************************************************************/
+#ifdef __cplusplus
+ extern "C" {
+#endif
+/********************************************************************************
+**                            Include Files | Start                            **
+********************************************************************************/
+/* CORE interfaces */
+#include "cosmosTypesBase.h"
+/********************************************************************************
+**                            Include Files | Stop                             **
+********************************************************************************/
+/********************************************************************************
+**                          Macro Definitions | Start                          **
+********************************************************************************/
+/********************************************************************************
+  * DOXYGEN START GROUP                                                        **
+  * *************************************************************************//**
+  * @defgroup Macros_cosmosTypesCfg_h Macros
+  * @ingroup Global_cosmosTypesCfg  
+  * @{    
+********************************************************************************/
+/********************************************************************************
+  * DOXYGEN STOP GROUP                                                         **
+  * *************************************************************************//**
+  * @}  
+  * Macros_cosmosTypesCfg_h  
+********************************************************************************/
+/********************************************************************************
+**                          Macro Definitions | Stop                           **
+********************************************************************************/
+/********************************************************************************
+**                              Typedefs | Start                               **
+********************************************************************************/
+/********************************************************************************
+  * DOXYGEN START GROUP                                                        **
+  * *************************************************************************//**
+  * @defgroup Typedefs_cosmosTypesCfgCfg_h Typedefs
+  * @ingroup Global_cosmosTypesCfg  
+  * @{    
+********************************************************************************/
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_PermissionsConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const BitWidthType * const bitLocksTasks;
+    const BitWidthType * const bitLocksTasksInverted;
+
+} CosmOS_PermissionsConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_BufferConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_GenericVoidType * const routeFunc;
+    const BitWidthType * const routeIdToFunc;
+    const BitWidthType * const routeIdToEntity;
+
+} CosmOS_RoutesConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_BufferConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    unsigned char * const buffer;
+    const BitWidthType size;
+    const CosmOS_PermissionsConfigurationType * const readPermission;
+    const CosmOS_PermissionsConfigurationType * const writePermission;
+
+} CosmOS_BufferConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_StackConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const BitWidthType stackSize;
+    const AddressType stackLowAddress;
+    const AddressType stackHighAddress;
+    
+} CosmOS_StackConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_TaskConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_StackConfigurationType * const stack;
+    const CosmOS_HandlerType handler;
+    const BitWidthType id;
+    const BitWidthType wcet;
+    const BitWidthType programId;
+    const BitWidthType coreId;
+    
+} CosmOS_TaskConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_ProgramConfigurationType struct type
+********************************************************************************/
+typedef struct            
+{               
+    const CosmOS_TaskConfigurationType * const tasks;
+    const BitWidthType coreId;
+    const BitWidthType numberOfTasks;
+    const BitWidthType programMemorySize;
+    const AddressType programMemoryLowAddress;
+    const AddressType programMemoryHighAddress;
+    
+} CosmOS_ProgramConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_ScheduleTableConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const BitWidthType startTick;
+    void * const taskVar;
+
+} CosmOS_ScheduleTableConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_SchedulerConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_ScheduleTableConfigurationType * const scheduleTable;
+    const BitWidthType hyperTick;
+    const BitWidthType scheduleTableElementsNum;
+    const BitWidthType lastToFirstTaskTicks;
+    const BitWidthType syncTicks;
+    const BitWidthType firstSyncTaskStartTick;
+    void * const idleTaskVar;
+
+} CosmOS_SchedulerConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_SysJobsGroupConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_GenericVoidType * const handlers;
+    const BitWidthType numOfHandlers;
+    const BitWidthType tickMultiplicator;
+
+} CosmOS_SysJobsGroupConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_SysJobsGroupConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_SysJobsGroupConfigurationType * const groups;
+    const BitWidthType numOfGroups;
+    const BitWidthType maxTickMultiplicator;
+
+} CosmOS_SysJobsConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_CoreConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_ProgramConfigurationType * const programs;
+    const BitWidthType numberOfPrograms;
+    const CosmOS_SchedulerConfigurationType * const scheduler;
+
+} CosmOS_CoreConfigurationType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_OsConfigurationType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_CoreConfigurationType * const cores;
+    const BitWidthType numberOfCores;
+    const CosmOS_BufferConfigurationType * const buffers;
+    const BitWidthType numberOfBuffers;
+    const CosmOS_RoutesConfigurationType * const route;
+    
+} CosmOS_OsConfigurationType;
+/********************************************************************************
+  * DOXYGEN STOP GROUP                                                         **
+  * *************************************************************************//**
+  * @}  
+  * Typedefs_cosmosTypesCfg_h  
+********************************************************************************/
+/********************************************************************************
+**                              Typedefs | Stop                                **
+********************************************************************************/
+#ifdef __cplusplus
+}
+#endif
+/********************************************************************************
+**                         END OF C++ SUPPORT SECTION                          **
+********************************************************************************/
+#endif
+/********************************************************************************
+**                           END OF THE HEADER FILE                            **
+********************************************************************************/
---
+++ cosmosTypes/inc/cosmosTypesVar.h
@@ -0,0 +1,223 @@
+/********************************************************************************
+**                                                                             **
+**                         COSMOS FILE | CREATED BY HUMAN                      **
+**                                                                             **
+*********************************************************************************
+**                       DOXYGEN DOCUMENTATION INFORMATION                     **
+*****************************************************************************//**
+** @file cosmosTypesVar.h                                      
+*********************************************************************************	
+<!--                   cosmosTypesVar Unit Group Definition                   -->
+********************************************************************************* 
+** @defgroup cosmosTypesVar_unit cosmosTypesVar Unit 
+** @ingroup cosmosTypes_module        
+** @brief cosmosTypesVar Unit 
+** @details lorem                               
+*********************************************************************************	
+<!--                           Version Information                            -->
+*********************************************************************************
+** @version 1.0.0
+** @date 1.8.2020
+** @author https://github.com/PavolKostolansky   
+*********************************************************************************	
+<!--                          Warnings and License                            -->
+*********************************************************************************
+** @warning Modifying code can lead to unexpected behaviour of the whole system
+** @copyright MIT License       	
+*********************************************************************************
+<!--              cosmosTypesVar Unit Global Group Definition                 -->
+*********************************************************************************	
+** @defgroup Global_cosmosTypesVar Global
+** @ingroup cosmosTypesVar_unit 
+** @brief cosmosTypesVar globals
+** @details lorem 
+********************************************************************************/ 
+/********************************************************************************
+**                           START OF THE HEADER FILE                          **
+********************************************************************************/
+#ifndef __COSMOSTYPESVAR_H__
+#define __COSMOSTYPESVAR_H__
+/********************************************************************************
+**                         START OF C++ SUPPORT SECTION                        **
+********************************************************************************/
+#ifdef __cplusplus
+ extern "C" {
+#endif
+/********************************************************************************
+**                            Include Files | Start                            **
+********************************************************************************/
+/* CORE interfaces */
+#include "cosmosTypesCfg.h"
+/********************************************************************************
+**                            Include Files | Stop                             **
+********************************************************************************/
+/********************************************************************************
+**                          Macro Definitions | Start                          **
+********************************************************************************/
+/********************************************************************************
+  * DOXYGEN START GROUP                                                        **
+  * *************************************************************************//**
+  * @defgroup Macros_cosmosTypesVar_h Macros
+  * @ingroup Global_cosmosTypesVar  
+  * @{    
+********************************************************************************/
+/********************************************************************************
+  * DOXYGEN STOP GROUP                                                         **
+  * *************************************************************************//**
+  * @}  
+  * Macros_cosmosTypesVar_h  
+********************************************************************************/
+/********************************************************************************
+**                          Macro Definitions | Stop                           **
+********************************************************************************/
+/********************************************************************************
+**                              Typedefs | Start                               **
+********************************************************************************/
+/********************************************************************************
+  * DOXYGEN START GROUP                                                        **
+  * *************************************************************************//**
+  * @defgroup Typedefs_cosmosTypesVarCfg_h Typedefs
+  * @ingroup Global_cosmosTypesVar  
+  * @{    
+********************************************************************************/
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_TaskVariableType struct type
+********************************************************************************/
+typedef struct
+{
+    CosmOS_BarrierStateType barrierState;
+    
+} CosmOS_BarrierVariableType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_BufferVariableType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_BufferConfigurationType * const cfg;
+    BitWidthType head;
+    BitWidthType tail;
+    BitWidthType fullCells;
+
+} CosmOS_BufferVariableType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_TaskVariableType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_StackConfigurationType * const cfg;
+    
+} CosmOS_StackVariableType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_TaskVariableType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_TaskConfigurationType * const cfg;
+    CosmOS_StackVariableType * const stackVar;
+    StackPointerType stackPointer;
+    CosmOS_TaskStateType taskState;
+    CosmOS_TaskExecutionStateType executionState;
+    
+} CosmOS_TaskVariableType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_ProgramVariableType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_ProgramConfigurationType * const cfg;
+    CosmOS_TaskVariableType * taskInCurrentContext;
+    CosmOS_TaskVariableType * const taskVars;
+
+} CosmOS_ProgramVariableType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_SchedulerVariableType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_SchedulerConfigurationType * const cfg;
+    BitWidthType currentTick;
+    BitWidthType scheduleTableIterator;
+    BitWidthType scheduleTableIteratorPrior;
+    CosmOS_SchedulerStateType schedulerState;
+    BitWidthType nextSyncTick;
+    CosmOS_BooleanType syncInitState;
+
+} CosmOS_SchedulerVariableType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_SysJobsGroupVariableType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_SysJobsConfigurationType * const cfg;
+    BitWidthType currentTick;
+
+} CosmOS_SysJobsVariableType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_CoreVariableType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_CoreConfigurationType * const cfg;
+    CosmOS_ProgramVariableType * programInCurrentContext;
+    CosmOS_ProgramVariableType * const programVars;
+    CosmOS_SchedulerVariableType * const schedulerVar;
+    CosmOS_BarrierVariableType * const barrierVars;
+    CosmOS_OsState osState;
+    CosmOS_SysJobsVariableType * const sysJobs;
+
+} CosmOS_CoreVariableType;
+
+/********************************************************************************
+  * DOXYGEN DOCUMENTATION INFORMATION                                          **
+  * *************************************************************************//**
+  * @brief  CosmOS_OsVariableType struct type
+********************************************************************************/
+typedef struct
+{
+    const CosmOS_OsConfigurationType * const cfg;
+    CosmOS_CoreVariableType * const coreVars;
+    CosmOS_BufferVariableType * const bufferVars;
+    
+} CosmOS_OsVariableType;
+/********************************************************************************
+  * DOXYGEN STOP GROUP                                                         **
+  * *************************************************************************//**
+  * @}  
+  * Typedefs_cosmosTypesVar_h  
+********************************************************************************/
+/********************************************************************************
+**                              Typedefs | Stop                                **
+********************************************************************************/
+#ifdef __cplusplus
+}
+#endif
+/********************************************************************************
+**                         END OF C++ SUPPORT SECTION                          **
+********************************************************************************/
+#endif
+/********************************************************************************
+**                           END OF THE HEADER FILE                            **
+********************************************************************************/
