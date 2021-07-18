b53b285b30e95357ce4ddde0824d2f882626f31c
PavolKostolansky <pavol.kostolansky@gmail.com>
2021-07-18 10:11:00 +0200

    test of the submodule commit on reference project

--- _testing/CIL/mock/CILcore/mock.cpp
+++ _testing/CIL/mock/CILcore/mock.cpp
@@ -10,5 +10,5 @@ std::unique_ptr<CILcore_MOCK> CILcore_TestFixture::_CILcoreMock;
 // fake lib functions
 void CILcore_setCoreVar(CosmOS_OsVariableType * osVar)
 {
-    CILcore_TestFixture::_CILcoreMock->CILcore_setCoreVar(osVar);
+		CILcore_TestFixture::_CILcoreMock->CILcore_setCoreVar(osVar);
 }
