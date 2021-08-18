fb150eedfbc02411a8e896709133084205611ddd
PavolKostolansky <pavol.kostolansky@gmail.com>
2021-08-18 21:18:27 +0200

    fixed tests

--- os/test/mock/osBoot/mock.cpp
+++ os/test/mock/osBoot/mock.cpp
@@ -10,5 +10,5 @@ std::unique_ptr<OsBoot_MOCK> OsBoot_TestFixture::_OsBootMock;
 // fake lib functions
 void osBoot_bootValidate()
 {
-    Os_TestFixture::_OsBootMock->osBoot_bootValidate();
+    OsBoot_TestFixture::_OsBootMock->osBoot_bootValidate();
 }
